20235e06f0a924ff6371fea78b7ea3afcd6b6399
Евгения Андреева <carna@inbox.ru>
2021-10-20 20:53:35 +0300

    Add daysinyear

--- DateTime.pas
+++ DateTime.pas
@@ -5,6 +5,18 @@ begin
   result:=not((year mod 4 <> 0) or  (year mod 100 = 0)and(year mod 400 <> 0));
 end;
 
+function SecondsInMinute(minute:integer):integer;
+begin
+  result:=60;
+end;
+
+function DaysInYear(year:integer):integer;
+begin
+  if IsLeapYear(year) then result:=366
+  else result:=366;
+end;
+
+
 begin
 var d: DateTime := DateTime.Now;
   writeln(d.Day,'.',d.Month,'.',d.Year);
@@ -14,4 +26,7 @@ var d: DateTime := DateTime.Now;
   println (IsLeapYear(800));
   println (IsLeapYear(2012));
   println (IsLeapYear(2013));
+  println (DaysInYear(2013));
+  println (DaysInYear(2012));
+   
 end.
